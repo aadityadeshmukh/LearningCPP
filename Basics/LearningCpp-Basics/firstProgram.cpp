8bb98271102af4e0761c60d8f549ebc26db3c6f6
Aaditya Deshmukh <aadityadeshmukh@gmail.com>
2021-09-29 15:20:23 +0530

    casting

--- Basics/LearningCpp-Basics/firstProgram.cpp
+++ Basics/LearningCpp-Basics/firstProgram.cpp
@@ -10,12 +10,15 @@ int main(){
 	string user;
 	// console in followed by input operator followed by the variable we are going to store the data
 	cout << "Please enter your name:";
-	cin >> user;
+	//cin >> user;
 	//cout is short for console output
 	// << output operator
 	// endl adds a new line
 	cout << "Hello, " << user << " Please enter your favourite food:";
 	string faveFood;
-	cin >> faveFood;
+	//cin >> faveFood;
 	cout << "Woah! My favourite food is " << faveFood << " too" << endl;
+	
+	int numEggs = 3;
+	cout << static_cast<int>(numEggs * 1.5) << endl;
 }
