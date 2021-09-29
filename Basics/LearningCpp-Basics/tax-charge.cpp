6c4dbea9fc0096733fe10355f90904d6bf286178
Aaditya Deshmukh <aadityadeshmukh@gmail.com>
2021-09-29 16:31:58 +0530

    tax-charge

---
+++ Basics/LearningCpp-Basics/tax-charge.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+const float TAX_RATE = 0.04;
+int main(){
+	
+	string name;
+	float price = 4.50;
+	float charge = 0.0, taxCharge = 0.0;
+	int qty = 0;
+	
+	cout << "Enter the number you wish to order:";
+	cin >> qty;
+	
+	
+	charge = price * qty;
+	taxCharge = charge * TAX_RATE;
+	
+	cout << "The tax charge is: $" << taxCharge << endl;
+	charge = charge + taxCharge;
+	cout << "Total charge: $" << charge << endl;
+	
+}
