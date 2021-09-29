0e5f3e63335cfcc898b8fe7a75db0a3540f31674
Aaditya Deshmukh <aadityadeshmukh@gmail.com>
2021-09-29 17:24:22 +0530

    committing random number generator challenge

--- Basics/LearningCpp-Basics/chips.cpp
+++ Basics/LearningCpp-Basics/chips.cpp
@@ -15,11 +15,23 @@ int main(){
 	int chipsInPile = 0;
 	int chipsTaken = 0;
 	
+	string playerNames[2];
+	cout << "Player 1: Enter your name:";
+	cin >> playerNames[0];
+	cout << "Player 2: Enter your name:";
+	cin >> playerNames[1];
+	
 	// generate a random number of starting chips
 	srand(time(0)); //seed
 	
 	chipsInPile = (rand() % MAX_PILE) + 1;
 	cout << "The game starts with: " << chipsInPile << " chips in pile." << endl;
-	cout << "Each player can take: " << static_cast<int> (chipsInPile * MAX_PER_TURN) << endl;
+	int maxPerTurn = chipsInPile * MAX_PER_TURN;
+	cout << "Each player can take: " << maxPerTurn << endl;
+	
+	// draw a random number of chips from the pile with max per turn criterion applied
+	
+	int player1Sel = (rand() % maxPerTurn) + 1;
+	cout << "Player 1 selects: " << player1Sel << endl;
 	
 }
---
+++ Notes/Learning C++/Arrays.md
@@ -0,0 +1,10 @@
+# Arrays
+
+- declaration syntax `<data-type> <variable-name> [<size>]`
+	- Can add values at declaration time like `int matrix[3] = {1,2,3}`
+	- Or can add index by index like `matrix[2] = 2`
+	- Arrays can be multidimensional
+- Metaphor: Apartment complex
+	- Everyone has the same address "123 sunset boulevard"
+	- But everyone stays at an indexed location
+	- Example: 123 sunset boulevard apartment 3
