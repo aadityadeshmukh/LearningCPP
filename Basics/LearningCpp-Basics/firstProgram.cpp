0987b0a80913f92ddbd3726a6dc65cee1f8eef27
Aaditya Deshmukh <aadityadeshmukh@gmail.com>
2021-09-29 12:45:43 +0530

    Setting up env and 1st commit

---
+++ Basics/LearningCpp-Basics/firstProgram.cpp
@@ -0,0 +1,12 @@
+// This is a comment
+/*This is also a comment*/
+//We inport some libraries first. These are just some predefined code segments
+#include <iostream>
+using namespace std;
+
+int main(){
+	//cout is short for console output
+	// << output operator
+	// endl adds a new line
+	cout << "Hello World!" << endl;
+}
